dca74f29d50cf6dcb67ba66c8c8c49ba7cc46caf
Guilherme Kenzo Watanabe Fujimura <gkwfuji@gmail.com>
2025-04-17 10:05:48 -0300

    Descrição do projeto e atulização nos programas de senha

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Projetos_python
-Meus projetos com python
+# Projetos em Python
+
+### Projeto 1 - Cotação de Moedas
+O projeto em um programa que utiliza uma API das cotação atual de uma moeda específica. O programa solicita ao usuário que insira o código da moeda desejada e, em seguida, exibe a cotação atual. O programa também possui um gráfico que mostra a variação das cotações nos últimos 30 dias.
+
+**Tecnologias utilizadas:**
+- Python
+- API
+- Matplotlib
+
+### Projeto 2 - Senhas
+O projeto possui um programa que gera senhas aleatórias com base em critérios definidos pelo usuário. O programa solicita ao usuário que insira o tamanho da senha desejada, se a senha deve conter letras maiúsculas, minúsculas, números e caracteres especiais, e quantas senhas devem ser geradas. O programa então gera as senhas e as exibe na tela. Além disso, possui outro programa que verifica o nível de seguraça de uma senha inserida pelo usuário.
+
+**Tecnologias utilizadas:**
+- Python
+- Random
+- String
--- Senhas/gerador.py
+++ Senhas/gerador.py
@@ -1,27 +1,27 @@
 import random
 import string
 
-def senha_fraca(comprimento):
+def senha_letras(comprimento):
     caracteres = string.ascii_letters
     senha = ''.join(random.choice(caracteres) for caracter in range(comprimento))
     return senha
 
-def senha_medio(comprimento):
+def senha_numeros(comprimento):
     caracteres = string.ascii_letters + string.digits
     senha = ''.join(random.choice(caracteres) for caracter in range(comprimento))
     return senha
 
-def senha_forte(comprimento):
+def senha_simbolos(comprimento):
     caracteres = string.ascii_letters + string.digits + string.punctuation
     senha = ''.join(random.choice(caracteres) for caracter in range(comprimento))
     return senha
 
-nivel_senha = input('Escolha o nível de senha aleatória:\n1. Fraca\n2. Médio\n3. Forte\n')
+nivel_senha = input('Escolha os critérios da senha aleatória:\n1. Apenas letras\n2. Letras e números\n3. Letras, números e caracteres especiais\n')
 comprimento = int(input('Digite o comprimento da senha: '))
 match nivel_senha:
     case '1':
-        print('A senha gerada é:', senha_fraca(comprimento))
+        print('A senha gerada é:', senha_letras(comprimento))
     case '2':
-        print('A senha gerada é:', senha_medio(comprimento))
+        print('A senha gerada é:', senha_numeros(comprimento))
     case '3':
-        print('A senha gerada é:', senha_forte(comprimento))
+        print('A senha gerada é:', senha_simbolos(comprimento))
