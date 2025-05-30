d11bf3f8c0376b0aed0ccf39f04c369bb92d7615
Guilherme Kenzo Watanabe Fujimura <gkwfuji@gmail.com>
2025-05-30 13:42:12 -0300

    v2 do sistema bancário
    Criação de funções de criar usuário e conta corrente

--- Sistema_Bancario/v2.py
+++ Sistema_Bancario/v2.py
@@ -1,15 +1,93 @@
 from datetime import datetime, timedelta
 
+# Váriaveis globais // globals variables
+
 saldo = 0
 extrato_historico = []
 saques_diarios = 0
 ultimo_saque_diario = None
 transacoes_diarias = 0
 ultima_transacao = None
+agencia = '0001'
+usuarios = {}
+conta = {}
 
-def deposito():
+def main(): # menu principal // main menu
+    while True:
+        opcao = int(input('''Escolha a operação desejada:
+
+        [1] Deposito
+        [2] Saque
+        [3] Extrato
+        [4] Criar usuário
+        [5] Criar conta
+        [0] Sair
+
+        => '''))
+
+        if opcao == 1:
+            print('Depósito')
+            deposito()
+        elif opcao == 2:
+            print('Saque')
+            saque()
+        elif opcao == 3:
+            print('Extrato')
+            extrato()
+        elif opcao == 4:
+            print('Criar usuário')
+            criar_usuario()
+        elif opcao == 5:
+            print('Criar conta')
+            criar_conta()
+        elif opcao == 0:
+            break
+        else:
+            print('Operação inválida, selecione novamente a operação desejada.')
+
+def criar_usuario(): # cadastro de usuário // user registration
+    global usuarios
+    nome = input('Digite seu nome:\n')
+    cpf = int(input('Digite o CPF(somente números):\n'))
+
+    if cpf < 11:
+        print('CPF inválido.')
+        return criar_usuario()
+
+    data_nascimento = input('Digite a sua data de nascimento seguindo o seguinte formato\ndd/mm/aaaa:\n')
+
+    if data_nascimento < 8:
+        print('Data de nascimento inválida.')
+        return criar_usuario()
+
+    endereco = input('Digite o endereço seguinto o seguinte formato\nlogradouro, n° - bairro - cidade/sigla do estado:\n')
+
+    if cpf in usuarios:
+        print('Usuário já cadastrado.')
+        return criar_usuario()
+    else:
+        usuarios.update({cpf: {'nome': nome, 'data_nascimento': data_nascimento, 'endereco': endereco}})
+        print('Usuário cadastrado com sucesso.')
+        return usuarios
+
+def criar_conta(): # criação de conta // account creation
+    global agencia, usuarios
+    # nr_conta = len(conta) + 1 # propricio a erros se tivesse que excluir contas
+    nr_conta = 1
+    cpf = int(input('Digite o CPF do usuário:\n'))
+
+    if cpf in usuarios:
+        conta.update({nr_conta:{'agencia': agencia, 'cpf': cpf}})
+        print('Conta criada com sucesso.')
+        nr_conta += 1
+        print(conta)
+    else:
+        print('Usuário não cadastrado.')
+        return criar_usuario()
+
+def deposito(): # depósito // deposit
     global saldo, extrato_historico, transacoes_diarias, ultima_transacao
-    deposito = float(input('Digite o valor que deseja depositar: '))
+    deposito = float(input('Digite o valor que deseja depositar:\n'))
     horario_atual = datetime.now()
 
     if ultima_transacao != horario_atual.day:
@@ -33,7 +111,7 @@ def deposito():
         print('O valor informado é inválido.')
         return deposito
 
-def saque():
+def saque(): # saque // withdraw
     global saques_diarios, saldo, extrato_historico, ultimo_saque_diario, transacoes_diarias, ultima_transacao
     horario_atual = datetime.now()
 
@@ -54,7 +132,7 @@ def saque():
         print('Limite diário de 3 saques atingido. Tente novamente amanhã.')
         return saque
 
-    saque = float(input('Digite o valor que deseja sacar: '))
+    saque = float(input('Digite o valor que deseja sacar:\n'))
 
     if saque > 0 and saque <= 500 and saque <= saldo:
         historico = f'Saque: R$ {saque} | {horario_atual.hour}:{horario_atual.minute}:{horario_atual.second} | {horario_atual.day}/{horario_atual.month}/{horario_atual.year}'
@@ -74,7 +152,9 @@ def saque():
         print('Valor de saque inválido.')
         return saque
 
-def extrato():
+def extrato(): # extrato // statement
+    global saldo, extrato_historico
+    print('Extrato')
     if extrato_historico:
         for operacao in extrato_historico:
             print(operacao)
@@ -83,26 +163,4 @@ def extrato():
         print('Nenhuma transação registrada.')
     print(f'Saldo atual: R$ {saldo}')
 
-while True:
-    opcao = int(input('''Escolha a operação desejada:
-
-    [1] Deposito
-    [2] Saque
-    [3] Extrato
-    [0] Sair
-
-    => '''))
-
-    if opcao == 1:
-        print('Depósito')
-        deposito()
-    elif opcao == 2:
-        print('Saque')
-        saque()
-    elif opcao == 3:
-        print('Extrato')
-        extrato()
-    elif opcao == 0:
-        break
-    else:
-        print('Operação inválida, selecione novamente a operação desejada.')
+main()
